2e59ea8fc5f783e0b68ef628c3439d0d012c0c5a
jarbaswolf <jarbaswolf@gmail.com>
2022-11-14 11:04:31 -0300

    Implementation update and delete

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,7 +10,7 @@ import model.entites.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("=== TEST 1: findById ===");
@@ -21,11 +22,23 @@ public class Program2 {
 		for(Department d : list) {
 			System.out.println(d);
 		}
-		
+		/*
 		System.out.println("=== TEST 3: Insert ===");
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id: "+newDepartment.getId());
+		*/
+		System.out.println("=== TEST 4: UPDATE ===");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update Completed");
+		
+		System.out.println("=== TEST 5: DELETE");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
